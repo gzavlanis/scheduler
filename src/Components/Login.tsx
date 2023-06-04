293334177810cacaaa567e1d3aefed279cbdb02f
gzavlanis <gzavlanis@gmail.com>
2023-06-04 16:16:17 +0300

    update

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -57,13 +57,40 @@ const Login: FC<SomeComponentProps> = ({ history }): JSX.Element => {
                             <div className="card-body">
                                 <h3 className="card-title text-center text-secondary mt-3">Login Form</h3>
                                 <form autoComplete="off" onSubmit={handleSubmit(login)}>
-                                    
+                                    <div className="mb-3 mt-4">
+                                        <label className="form-label">Username</label>
+                                        <input type="email" className="form-control shadow-none" id="exampleFormControlInput1" {...register("username", { required: "Username is required!" })}/>
+                                        {errors.username && (
+                                            <p className="text-danger" style={{ fontSize: 14 }}>
+                                                {errors.username.message}
+                                            </p>
+                                        )}
+                                    </div>
+                                    <div className = "mb-3">
+                                        <label className = "form-label">Password</label>
+                                        <input type = "password" className = "form-control shadow-none" id = "exampleFormControlInput2"
+                                            {...register("password", { required: "Password is required!", })}
+                                        />
+                                        {errors.password && (
+                                            <p className = "text-danger" style = {{ fontSize: 14}}>
+                                                {errors.password.message}
+                                            </p>
+                                        )}
+                                    </div>
+                                    <div className="text-center mt-4 ">
+                                        <button className="btn btn-outline-primary text-center shadow-none mb-3" type="submit">Submit</button>
+                                    </div>
+                                    <p className = "card-text pb-2">Have an Account?{" "}
+                                        <Link style = {{ textDecoration: "none" }} to = {"/register"}>Sign Up</Link>
+                                    </p>
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <ToastContainer position = "top-right" autoClose = {5000} hideProgressBar closeOnClick rtl = {false} pauseOnFocusLoss = {false} draggable = {false} pauseOnHover limit = {1} transition = {Flip}/>
         </>
     );
-}
+};
+export default Login;
