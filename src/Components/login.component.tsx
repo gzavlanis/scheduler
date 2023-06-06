1c67bea28c3c2e33bc3d71e93722137515878017
gzavlanis <gzavlanis@gmail.com>
2023-06-06 21:05:52 +0300

    updates

--- src/Components/login.component.tsx
+++ src/Components/login.component.tsx
@@ -5,7 +5,7 @@ import * as Yup from "yup";
 import AuthService from "../services/auth.service";
 
 type Props = {};
-type State = {redirect: string | null, username: string, password: string, loading: boolean, message: string };
+type State = { redirect: string | null, username: string, password: string, loading: boolean, message: string };
 
 export default class Login extends Component<Props, State> {
     constructor(props: Props) {
@@ -40,8 +40,8 @@ export default class Login extends Component<Props, State> {
                 this.setState({
                     redirect: "/profile"
                 });
-            }, error => {
-                const resMessage = (error.response && error.response.data && error.response.data.message) || error.toString();
+            }, (error: any) => {
+                const resMessage = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
                 this.setState({ loading: false, message: resMessage });
             }
         );
@@ -55,6 +55,38 @@ export default class Login extends Component<Props, State> {
         const initialValues = {
             username: "", password: "",
         };
-        return ();
+        return (
+            <div className = "col-md-12">
+                <div className = "card card-container">
+                    <Formik initialValues = {initialValues} validationSchema = {this.validationSchema} onSubmit = {this.handleLogin}>
+                        <Form>
+                            <div className = "form-group">
+                                <label htmlFor = "username">Username</label>
+                                <Field name = "username" type = "text" className = "form-control"/>
+                                <ErrorMessage name = "username" component = "div" className = "alert alert-danger"/>
+                            </div>
+                            <div className = "form-group">
+                                <label htmlFor = "password">Password</label>
+                                <Field name = "password" type = "password" className = "form-control"/>
+                                <ErrorMessage name = "password" component = "div" className = "alert alert-danger"/>
+                            </div>
+                            <div className = "form-group">
+                                <button type = "submit" className = "btn btn-primary btn-block" disabled = {loading}>
+                                    {loading && (
+                                        <span className = "spinner-border spinner-border-sm"></span>
+                                    )}
+                                    <span>Login</span>
+                                </button>
+                            </div>
+                            {message && (
+                                <div className = "form-group">
+                                    <div className = "alert alert-danger" role = "alert">{message}</div>
+                                </div>
+                            )}
+                        </Form>
+                    </Formik>
+                </div>
+            </div>
+        );
     }
-}
+};
---
+++ src/Components/register.component.tsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import AuthService from "../services/auth.service";
+
+type Props = {};
+type State = {
+    
+};
