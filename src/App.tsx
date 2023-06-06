6382ddc1f2d252be5a74ef583686be5b563ab23a
gzavlanis <gzavlanis@gmail.com>
2023-06-07 00:12:08 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { Component }from "react";
+import { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AuthService from "./services/auth.service";
+import IUser from './types/user.type';
+import Login from "./Components/login.component";
+import Register from "./Components/register.component";
+import Home from "./Components/home.component";
+import Profile from "./Components/profile.component";
+import BoardUser from "./Components/board-user.component";
+import BoardModerator from "./Components/board-moderator.component";
+import BoardAdmin from "./Components/board-admin.component";
+import EventBus from "./common/EventBus";
+
+type Props = {};
+
+type State = { showModeratorBoard: boolean, showAdminBoard: boolean, currentUser: IUser | undefined }
 
 class App extends Component<Props, State> {
-  render() {
-    
-  }
+  
 }
 
 export default App;
